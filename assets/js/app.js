21ac9c7964b2ebb023c1a809395b56e65c26ad8a
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-07-13 16:01:32 -0500

    jala elementos json y los imprime en consola, creacion de tarjetas dinamicamente

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,38 @@ function getJSON(url2){
   });
 }
 
-getJSON("data/earth-like-results.json")
-  .then(function(mensaje){return(getJSON(mensaje.results[0]))})
-  .then(function(resultado){console.log(resultado)})
+var arregloPlanetas = [];
+  getJSON("data/earth-like-results.json").then(function(respuesta){
+
+    for(var i = 0; i < respuesta.results.length ; i++){
+      arregloPlanetas.push(getJSON(respuesta.results[i]));
+      arregloPlanetas[i].then(function(planeta){
+        console.log(planeta);
+        obtenerDatos(planeta);
+      });
+    }
+  });
+
+  var obtenerDatos = function(planeta){
+    var nombre = planeta.pl_name;
+    var telescope = planeta.pl_telescope;
+    var masa = planeta.pl_masse;
+    var fecha = planeta.pl_disc;
+  }
+
+//crear tarjeta
+var contenedorTarjetas = document.getElementById("contenedor-tarjetas");
+var row = document.createElement("div");
+var col_s12 = document.createElement("div");
+var card = document.createElement("div");
+var card_image = document.createElement("div");
+var card__image_image = document.createElement("img");
+var card_content = document.createElement("div");
+var card__content_title = document.createElement("span");
+var card__content_parrafo = document.createElement("p");
+
+contenedorTarjetas.setAttribute("class", "center-align");
+row.setAttribute("class", "row");
+col_s12.setAttribute("class", "col s12 m7");
+card.setAttribute("class", "card");
+card_image.setAttribute("class", "card-image");
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.use("/data", express.static(__dirname + "/data"));
 app.use("/static", express.static(__dirname + "/assets"));
+app.use("/static", express.static(__dirname + "/node_modules"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
