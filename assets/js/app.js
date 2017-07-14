e90c8a907c44d0f189635db474800c8bf18bd2e0
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-07-14 08:56:33 -0500

    creacion de plantilla

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,19 +33,44 @@ var arregloPlanetas = [];
     var fecha = planeta.pl_disc;
   }
 
-//crear tarjeta
-var contenedorTarjetas = document.getElementById("contenedor-tarjetas");
-var row = document.createElement("div");
-var col_s12 = document.createElement("div");
-var card = document.createElement("div");
-var card_image = document.createElement("div");
-var card__image_image = document.createElement("img");
-var card_content = document.createElement("div");
-var card__content_title = document.createElement("span");
-var card__content_parrafo = document.createElement("p");
+var crearPlaneta = function(planeta){
+  obtenerDatos();
 
-contenedorTarjetas.setAttribute("class", "center-align");
-row.setAttribute("class", "row");
-col_s12.setAttribute("class", "col s12 m7");
-card.setAttribute("class", "card");
-card_image.setAttribute("class", "card-image");
+  console.log("hola");
+
+  //crear tarjeta
+  var contenedorTarjetas = document.getElementById("contenedor-tarjetas");
+  var row = document.createElement("div");
+  var col_s12 = document.createElement("div");
+  var card = document.createElement("div");
+  var card_image = document.createElement("div");
+  var card__image_image = document.createElement("img");
+  var card_content = document.createElement("div");
+  var card__content_title = document.createElement("span");
+  var card__content_parrafo = document.createElement("p");
+
+  contenedorTarjetas.setAttribute("class", "center-align");
+  row.setAttribute("class", "row");
+  col_s12.setAttribute("class", "col s12 m7");
+  card.setAttribute("class", "card");
+  card_image.setAttribute("class", "card-image");
+  card__image_image.setAttribute("src", "http://www.dummyimage.com/300x300");
+  card_content.setAttribute("class", "card_content");
+  card__content_title.setAttribute("class", "card-title");
+
+  //contenido
+  card__content_title.innerHTML = nombre;
+  card__content_parrafo.innerHTML = "Fecha de descubrimiento:" + fecha +
+                                    "<br> Telescope:" + telescope +
+                                    "<br> Masa:" + masa +
+                                    "<br> Descubrimiento:" + fecha;
+
+  card_content.appendChild(card_content_parrafo);
+  card_content.appendChild(card__content_title);
+  card_image.appendChild(card_image_image);
+  card.appendChild(card_image);
+  card.appendChild(card_content);
+  col.appendChild(card);
+  contenedorTarjetas.appendChild(col);
+
+}
